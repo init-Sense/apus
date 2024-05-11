23df8ce61538e5a50179038533b4a252d8588344
bottolo <samuele.castiglia.98@gmail.com>
2024-05-11 15:42:18 +0200

    script: Player script with snapping feature

--- scripts/player/player.gd
+++ scripts/player/player.gd
@@ -1,81 +1,102 @@
 extends CharacterBody3D
 
-# Movement properties
-var speed: float           = 5.0
+var speed
 const WALK_SPEED: float    = 5.0
 const SPRINT_SPEED: float  = 8.0
-const JUMP_VELOCITY: float = 4.5
-const GRAVITY: float       = 7.0
-const SENSITIVITY: float   = 0.03
+const JUMP_VELOCITY: float = 4.8
+const SENSITIVITY: float   = 0.004
+const BOB_FREQ: float      = 2.4
+const BOB_AMP: float       = 0.08
+var t_bob: float           = 0.0
+const BASE_FOV: float      = 75.0
+const FOV_CHANGE: float    = 1.5
+var gravity: float         = 9.8
+
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
 @onready var snapshot_viewport: SubViewport = $Viewport/SnapshotViewport
 @onready var snapshot_camera: Camera3D = $Viewport/SnapshotViewport/SnapshotCamera3D
 @onready var main_camera_display_plane: MeshInstance3D = $Head/MainCameraDisplayPlane
-@onready var player_mesh: MeshInstance3D = $MeshInstance3D
-const PLAYER_LAYER: int                  = 2  # Example layer number for the player's mesh
-const SNAPSHOT_CAMERA_MASK: int          = ~(1 << PLAYER_LAYER)  # Mask excluding player's layer
 
 
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-    main_camera_display_plane.visible = false
+
+func _unhandled_input(event):
+    if event is InputEventMouseMotion:
+        head.rotate_y(-event.relative.x * SENSITIVITY)
+        camera.rotate_x(-event.relative.y * SENSITIVITY)
+        camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
 
 
 func snap_image():
-    player_mesh.visible = false
-    main_camera_display_plane.visible = false
-
     snapshot_camera.global_transform = camera.global_transform
 
+    snapshot_viewport.set_world_3d(get_world_3d())
     var snapshot_image := snapshot_viewport.get_texture().get_image()
 
     var snapshot_texture := ImageTexture.new()
     snapshot_texture.set_image(snapshot_image)
 
-    var material := StandardMaterial3D.new()
+    var snapshot_display := MeshInstance3D.new()
+    var plane_mesh       := PlaneMesh.new()
+    plane_mesh.size = Vector2(1.0, 0.5625)
+
+    var material: StandardMaterial3D = StandardMaterial3D.new()
     material.albedo_texture = snapshot_texture
-    main_camera_display_plane.material_override = material
+    snapshot_display.material_override = material
+    snapshot_display.mesh = plane_mesh
 
-    main_camera_display_plane.visible = true
+    snapshot_display.global_transform = camera.global_transform * Transform3D.IDENTITY.translated(Vector3(0, 0, -2))
+    snapshot_display.rotation_degrees = Vector3(randi_range(50, 170), randi_range(50, 170), randi_range(50, 170),)
 
-    player_mesh.visible = true
+    var snapshot_container: Node = get_node("/root/Level/SnapshotContainer")
+    if snapshot_container:
+        snapshot_container.add_child(snapshot_display)
+    else:
+        print("SnapshotContainer not found.")
 
 
-# Function to handle player movement
-func _physics_process(delta: float):
+func _physics_process(delta):
     if not is_on_floor():
-        velocity.y -= GRAVITY * delta
+        velocity.y -= gravity * delta
 
-    if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+    if Input.is_action_pressed("sprint"):
+        speed = SPRINT_SPEED
+    else:
+        speed = WALK_SPEED
 
-    speed = SPRINT_SPEED if Input.is_action_pressed("sprint") else WALK_SPEED
-
-    var input_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
-    var direction    := (head.transform.basis * Vector3(input_vector.x, 0, 0)).normalized()
-
+    var input_dir: Vector2 = Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
+    var direction: Vector3 = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     if is_on_floor():
-        velocity.x = direction.x * speed
-        velocity.z = direction.z * speed
+        if direction:
+            velocity.x = direction.x * speed
+            velocity.z = direction.z * speed
+        else:
+            velocity.x = lerp(velocity.x, direction.x * speed, delta * 7.0)
+            velocity.z = lerp(velocity.z, direction.z * speed, delta * 7.0)
     else:
         velocity.x = lerp(velocity.x, direction.x * speed, delta * 3.0)
-        velocity.z = lerp(velocity.x, direction.z * speed, delta * 3.0)
+        velocity.z = lerp(velocity.z, direction.z * speed, delta * 3.0)
+
+    t_bob += delta * velocity.length() * float(is_on_floor())
+    camera.transform.origin = _headbob(t_bob)
+
+    var velocity_clamped = clamp(velocity.length(), 0.5, SPRINT_SPEED * 2)
+    var target_fov       = BASE_FOV + FOV_CHANGE * velocity_clamped
+    camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
     move_and_slide()
 
 
-# Function to handle camera rotation using mouse movement
-func _unhandled_input(event: InputEvent):
-    if event is InputEventMouseMotion:
-        head.rotate_y(-event.relative.x * SENSITIVITY)
-        camera.rotate_x(-event.relative.y * SENSITIVITY)
-
-        camera.rotation_degrees.x = clamp(camera.rotation_degrees.x, -40, 60)
+func _headbob(time) -> Vector3:
+    var pos: Vector3 = Vector3.ZERO
+    pos.y = sin(time * BOB_FREQ) * BOB_AMP
+    pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
+    return pos
 
 
-# Function to handle snapping events
 func _process(delta: float):
     if Input.is_action_just_pressed("snap"):
         snap_image()
